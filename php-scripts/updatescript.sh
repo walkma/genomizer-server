42537badd04b5abe8b135eeb68bf63005fb81d6a
pvt14server <pvt14server@mailinator.com>
2014-05-02 12:42:55 +0200

    super updater script of doom

---
+++ php-scripts/updatescript.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+rm -f upload.php
+rm -f download.php
+
+cp /var/www/html/upload.php /home/pvt/workspace/genomizer-server/php-scripts/
+cp /var/www/html/download.php /home/pvt/workspace/genomizer-server/php-scripts/
