2122a264cc94a2b3d01babb9b257d36a80564e91
dv12tkn <dv12tkn@weevil.cs.umu.se>
2014-05-16 10:57:03 +0200

    - Added !!ALOT!! of variables

--- src/geo/GEOFileTuple.java
+++ src/geo/GEOFileTuple.java
@@ -1,7 +1,64 @@
 package geo;
 
+
 public class GEOFileTuple {
 
+	public String seriesRelation;
+	public String seriesSampleTaxid;
+	public String seriesPlatformTaxid;
+	public String seriesPlatformId;
+	public String seriesContactCountry;
+	public String seriesContactPostalCode;
+	public String seriesContactState;
+	public String seriesContactCity;
+	public String seriesContactAddress;
+	public String seriesContactInstitute;
+	public String seriesContactLaboratory;
+	public String seriesContactPhone;
+	public String seriesContactEmail;
+	public String seriesContactName;
+	public String seriesSampleId;
+	public String[] seriesContributors;
+	public String seriesType;
+	public String seriesOverallDesign;
+	public String[] seriesSummary;
+	public String seriesWebLink;
+	public String seriesLastUpdateDate;
+	public String seriesSubmissionDate;
+	public String seriesStatus;
+	public String seriesGeoAccession;
+	public String seriesTitle;
+	public String ID_REF;
+	public String sampleSupplementaryFile;
+	public String sampleRelation;
+	public String sampleLibraryStrategy;
+	public String sampleLibrarySource;
+	public String sampleLibrarySelection;
+	public String sampleInstrumentModel;
+	public String sampleContactPostalCode;
+	public String sampleContactState;
+	public String sampleContactCity;
+	public String sampleContactAddress;
+	public String sampleContactInstitute;
+	public String sampleContactPhone;
+	public String sampleContactEmail;
+	public String sampleContactName;
+	public String samplePlatformId;
+	public String sampleDataProcessing;
+	public String sampleDescription;
+	public String sampleTaxid;
+	public String sampleExtractProtocol;
+	public String sampleMolecule;
+	public String sampleCharacteristics;
+	public String sampleOrganism;
+	public String sampleSourceName;
+	public String sampleChannelCount;
+	public String sampleType;
+	public String sampleLastUpdateDate;
+	public String sampleSubmissionDate;
+	public String sampleStatus;
+	public String sampleGeoAccession;
+	public String sampleTitle;
 
 	public GEOFileTuple() {
 
