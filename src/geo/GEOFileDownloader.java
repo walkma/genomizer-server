e32e12f73e3b6326ce422d7cba635c1dea892881
oi11msd <oi11msd@cs.umu.se>
2014-05-20 11:29:43 +0200

    Added stuff

--- src/geo/GEOFileDownloader.java
+++ src/geo/GEOFileDownloader.java
@@ -1,18 +1,33 @@
 package geo;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 public class GEOFileDownloader {
@@ -20,7 +35,6 @@ public class GEOFileDownloader {
 
 
 	public static String getSRAFromDir(String url) throws IOException {
-		url = url + "/";
 		// This will get the subfolders from the url
 		InputStream is = new URL(url).openStream();
 		byte[] buffer = new byte[1024];
@@ -43,35 +57,29 @@ public class GEOFileDownloader {
 	}
 
 	public static void downloadFile(String url, String tempFileLoc) throws MalformedURLException, IOException {
-
-        // new HttpClient
-        HttpClientBuilder hcBuilder = HttpClients.custom();
-        CloseableHttpClient httpClient = hcBuilder.build();
-
-        // post header
-		HttpPost httpPost = new HttpPost("scratchy.cs.umu.se:8000/download_from_geo.php?url=" + url + "&path=" + tempFileLoc);
-
 		try {
-            HttpResponse response;
-            // execute HTTP post request
-            response = httpClient.execute(httpPost);
-            HttpEntity resEntity = response.getEntity();
-            System.out.println("Response code: "
-                    + response.getStatusLine().getStatusCode());
-            if (resEntity != null) {
+			URL targetUrl = new URL(
+					"http://scratchy.cs.umu.se:8000/download_from_geo.php?" + "url=" + url + "&path=" + tempFileLoc);
+			HttpURLConnection conn = (HttpURLConnection) targetUrl
+					.openConnection();
 
-                String responseStr = EntityUtils.toString(resEntity).trim();
-                System.out.println("Response: " + responseStr);
-            }
-        } catch (ClientProtocolException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch(FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+			int responseCode;
+			if ((responseCode = conn.getResponseCode()) != 200) {
+				System.out
+				.println("Error wrong response code: " + responseCode);
+			}
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					conn.getInputStream()));
+			while (in.ready()) {
+				System.out.println(in.readLine());
+			}
+			conn.disconnect();
 
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Connection error: " + e.getMessage() + " "
+					+ url);
+		}
 	}
 }
